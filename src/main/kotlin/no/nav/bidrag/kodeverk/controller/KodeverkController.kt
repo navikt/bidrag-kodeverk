a7f9aa62a41d0bb9969d8e16a91a206e1f29eee8
Ugur Alpay Cenar <cenar93@gmail.com>
2024-12-15 12:16:47 +0100

    Initial setup

--- src/main/kotlin/no/nav/bidrag/kodeverk/controller/KodeverkController.kt
+++ src/main/kotlin/no/nav/bidrag/kodeverk/controller/KodeverkController.kt
@@ -6,13 +6,13 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import no.nav.bidrag.commons.service.KodeverkKoderBetydningerResponse
 import no.nav.bidrag.kodeverk.service.KodeverKService
-import no.nav.security.token.support.core.api.Protected
+import no.nav.security.token.support.core.api.Unprotected
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@Protected
+@Unprotected
 class KodeverkController(
     val kodeverKService: KodeverKService,
 ) {
