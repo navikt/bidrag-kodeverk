f608bb59464ddef55a6325baa2ea97f97a7596a9
Simen Høston <simen.naper.hoston@webstep.no>
2026-05-26 12:50:58 +0200

    SB4

--- src/test/kotlin/no/nav/bidrag/kodeverk/SpringTestRunner.kt
+++ src/test/kotlin/no/nav/bidrag/kodeverk/SpringTestRunner.kt
@@ -1,26 +1,29 @@
 package no.nav.bidrag.kodeverk
 
-import StubUtils
 import com.github.tomakehurst.wiremock.WireMockServer
 import no.nav.bidrag.commons.web.test.HttpHeaderTestRestTemplate
+import no.nav.bidrag.kodeverk.utils.StubUtils
 import no.nav.security.token.support.spring.test.EnableMockOAuth2Server
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureTestRestTemplate
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.server.LocalServerPort
-import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock
 import org.springframework.context.ApplicationContext
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.wiremock.spring.ConfigureWireMock
+import org.wiremock.spring.EnableWireMock
 
 @ExtendWith(SpringExtension::class)
 @ContextConfiguration(classes = [BidragKodeverkLocal::class])
 @SpringBootTest(classes = [BidragKodeverkLocal::class, StubUtils::class], webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureWireMock(port = 0)
 @ActiveProfiles("test")
 @EnableMockOAuth2Server
+@EnableWireMock(ConfigureWireMock(port = 0))
+@AutoConfigureTestRestTemplate
 class SpringTestRunner {
     @LocalServerPort
     protected var port: Int = 0
@@ -47,8 +50,6 @@ class SpringTestRunner {
     }
 
     fun rootUri(): String = LOCALHOST + port
-
-    protected fun getPort(): String = port.toString()
 
     companion object {
         private const val LOCALHOST = "http://localhost:"
